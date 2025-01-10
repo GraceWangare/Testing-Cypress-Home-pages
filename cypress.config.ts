ab273f3d54e2e88decbae519787045819c02d02a
Grace Kungu <106985009+GraceWangare@users.noreply.github.com>
2025-01-11 00:08:05 +0100

    changes for pipeline runs

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    reporter: 'mocha-awesome',
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporterOptions: {
+      reportDir: 'cypress/reports', 
+      overwrite: false, 
+      html: true, 
+      json: true,
+     }
   },
 });
