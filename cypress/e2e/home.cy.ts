3bfc9ae93c2fbe44f0a46e135a16e795648eabf7
Grace Kungu <106985009+GraceWangare@users.noreply.github.com>
2025-01-11 00:15:57 +0100

    tag test cases from azure

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,14 +4,14 @@ describe('home page', () => {
     cy.visit('http://localhost:3000/')
 
   })
-  it(' verify the Hi elemnt contains the correct text', () => {
+  it(' verify the Hi elemnt contains the correct text @36', () => {
     
     cy.get('[data-test="hero-heading"]')
       .should("exist")
       .contains('Testing Next.js Applications with Cypress')
   })
 
-  it.only(' verify the features on the homepage are correct E1', () => {
+  it.only(' verify the features on the homepage are correct E1 @37', () => {
     
     cy.get('dt').eq(0).contains("4 Courses")
     cy.get('dt').eq(1).contains("25+ Lessons")
