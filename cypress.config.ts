8b0ff71515ea18083da0bafda3f2015277db404f
Grace Kungu <106985009+GraceWangare@users.noreply.github.com>
2025-01-15 09:44:28 +0100

    changes to run in pipeline

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'xx6sn4',
   e2e: {
-    reporter: 'mocha-awesome',
+    reporter: 'junit',
     
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     reporterOptions: {
-      reportDir: 'cypress/reports', 
-      overwrite: false, 
-      html: true, 
-      json: true,
+      mochaFile: 'results/test-results-[hash].xml', 
+      toConsole:false
+       
+     
+      
      }
   },
 });
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,3 +1,5 @@
+import {faker} from "@faker-js/faker";
+
 describe('home page', () => {
 
   beforeEach(() =>{
